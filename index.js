ff66633831d23a86b5e42e7aaf4bff18c2699294
Andrew <andrew_3586@iprimus.com.au>
2022-12-16 14:09:25 +1100

    finalised

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (spy) {
+    return spy()
+
+  }
+  function returnsANamedFunction (){
+  return function andrew(){
+  }
+
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){
+
+    }
+}
